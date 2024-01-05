59a8be07dc14691525fa8f53faf6eb060a389cce
Siddhant Kalamkar <102784516+siddhantK0905@users.noreply.github.com>
2024-01-05 12:50:17 +0530

    Create 994. Rotting Oranges.cpp

---
+++ Graph/Medium/994. Rotting Oranges.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int tm =0;
+        queue<pair< pair<int,int>,int>> q;
+        int n = grid.size();
+        int m = grid[0].size();
+        int vis[n][m];
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j] == 2){
+                    q.push({{i,j},0});
+                    vis[i][j] = 2;
+                }
+                else{
+                    vis[i][j] = 0;
+                }
+            }
+        }
+
+        int dcol[] = {0,1,0,-1};
+        int drow[] = {-1,0,1,0}; 
+
+        while(!q.empty()){
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int t = q.front().second;
+            q.pop();
+            tm = max(tm, t);
+            
+            for(int i=0; i<4; i++){
+                int nrow = r+ drow[i];
+                int ncol = c+dcol[i];
+
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                    grid[nrow][ncol] == 1 && vis[nrow][ncol] == 0 ){
+                        q.push({{nrow, ncol},t+1});
+                        vis[nrow][ncol] =2;
+                    }
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(vis[i][j] == 0 && grid[i][j] == 1){
+                    return -1;
+                }
+            }
+        }
+
+        return tm;
+
+
+    }
+};
+
+
+//Space Complexity O(m*n)
+//Time Complexity O(m*n)
