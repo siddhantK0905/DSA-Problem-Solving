f256bcb31779710b9e7eca23c3361aeb7732847a
Siddhant Kalamkar <102784516+siddhantK0905@users.noreply.github.com>
2024-02-19 22:55:21 +0530

    Math
    19-02-2024

---
+++ LeetCode Daily Challenge/231. Power of Two (Easy) .cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+        for (int i=0;i<31;i++){
+            
+           int ans=pow(2,i);
+            if(ans==n){
+                
+                return true;
+            }            
+        }
+        return false;
+        
+    }
+};
+
+//TC - O(1)
+//SC - O(1)
